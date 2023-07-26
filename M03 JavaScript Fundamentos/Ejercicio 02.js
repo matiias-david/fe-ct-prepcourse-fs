8167badb3921d8e82f154f4cc52e3d24bc8c9d41
Matias Parra <maty_lasirenaa@hotmail.com>
2023-07-25 21:14:22 -0300

    Ejercicio 3

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,34 +4,51 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
-}
+   return string;
+} 
+devolverString("Hola");
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var resultado = x + y;
+   return resultado;
 }
+suma(1, 2)
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var resultado = x - y;
+   return resultado;
 }
+resta(1, 2)
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   resultado = x / y;
+   return resultado;
 }
+divide(2, 2)
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   resultado = x * y;
+   return resultado;
 }
+multiplica(1, 1)
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
-}
+   resultado = x % y;
+   return resultado;
+} 
+obtenerResto(2, 2);
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
